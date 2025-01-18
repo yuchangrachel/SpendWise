f76fceb4eb47c12761112db85e53726f8cd33ddd
yuchangrachel <yheimbusi@gmail.com>
2025-01-17 16:10:34 -0800

    auth api testing

--- backend/app/models.py
+++
@@ -1,6 +0,0 @@
-from . import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
